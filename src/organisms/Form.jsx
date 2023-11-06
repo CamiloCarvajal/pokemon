7b8cce4210410c0c03c0c5c0f0896163294da322
camilo carvajal <camilocc597@gmail.com>
2023-11-06 00:47:58 -0500

    organisms

---
+++ src/organisms/Form.jsx
@@ -0,0 +1,9 @@
+import SearchForm from "../molecules/SearchForm"
+
+function Form({ setName, getInfo }) {
+    return (
+        <SearchForm setName={setName} getInfo={getInfo} />
+    )
+}
+
+export default Form
---
+++ src/organisms/Generation.jsx
@@ -0,0 +1,11 @@
+import GenerationList from "../molecules/GenerationList";
+
+function Generation({generations}) {
+    // console.log(generations)
+    return (
+        // <h3>Hola</h3>
+        <GenerationList generations = {generations}/>
+    )
+}
+
+export default Generation
